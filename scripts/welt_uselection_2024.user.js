668b99634eb514008db5595f61d0f76123c5be3d
tlvsion <thorsten.lucke@v-sion.de>
2024-09-25 18:02:54 +0200

    foreach eventlist

--- scripts/welt_uselection_2024.user.js
+++ scripts/welt_uselection_2024.user.js
@@ -24,9 +24,9 @@ console.log('Load Tampermonkey script');
  */
 const eventList = []
 const stateList = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-for (const state in stateList) {
+stateList.forEach((state) => {
   eventList.push({ text: state, callback: () => webSocket.send({ action: 'set current state', statePostal: state, sendData: 'flowics' })})
-}
+})
 
 ////////////////////
 //// Websockets ////
