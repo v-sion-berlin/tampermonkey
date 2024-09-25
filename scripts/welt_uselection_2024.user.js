5e487a43bff2e77787394e21b0612d89efa41760
Andreas Müller <andreas.mueller@v-sion.de>
2024-09-25 16:52:58 +0200

    bugfix

--- scripts/welt_uselection_2024.user.js
+++ scripts/welt_uselection_2024.user.js
@@ -24,7 +24,7 @@ console.log('Load Tampermonkey script');
  */
 const eventList = []
 const stateList = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
-for (state in stateList) {
+for (const state in stateList) {
   eventList.push({ text: state, callback: () => webSocket.send({ action: 'set current state', statePostal: state, sendData: 'flowics' })})
 }
 
